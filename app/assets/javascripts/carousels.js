690cb170fe8579463cad398b6be4e92081e10ea3
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-08-06 18:32:13 -0400

    Basic carousel implementation

---
+++ app/assets/javascripts/carousels.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  // normal view
+  $('.scroller').slick({
+    infinite: true,
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    arrows: false,
+    slidesToScroll: 1
+  });
+});
