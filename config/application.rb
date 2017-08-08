af5c339ce35ea634e785a6bfad2d896a424b0c27
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-08-08 13:25:49 -0400

    Update rails version

--- config/application.rb
+++ config/application.rb
@@ -30,7 +30,6 @@ module BryanTormey
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
     config.filestack_rails.api_key = ENV['FILEPICKER_KEY']
   end
 end
