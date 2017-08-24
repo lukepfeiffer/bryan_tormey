7f1c59e41237851c5fb010d36ecd468bd064d929
Luke Pfeiffer <lukerpfeiffer@gmail.com>
2017-08-24 14:03:46 -0400

    Remove focus on images

--- app/assets/javascripts/carousels.js
+++ app/assets/javascripts/carousels.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  // normal view
   $('.scroller').slick({
     infinite: true,
     speed: 1000,
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: false,
+    focusOnSelect: false,
     slidesToScroll: 1
   });
 
